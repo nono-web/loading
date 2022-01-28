85334f47f7ce14d38a9c8692bc897ecfd7e1075f
[REM-FR]Arnaud Didier <84334523+nono-web@users.noreply.github.com>
2022-01-28 10:29:29 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <span></span>
+      <span></span>
+      <span></span>
+     <h2>Loading...</h2>
+    </div>
+  );
+}
+
+export default App;
